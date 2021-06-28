4b6428e443a3199e0380ab48810a0800d6289665
jso <john.so@nordicsemi.no>
2021-06-28 18:38:27 +0800

    Add esb_demo_05R

---
+++ applications/esb_demo/esb_demo_05R/lib/radio_ipc.h
@@ -0,0 +1,42 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#ifndef ESB_IPC_H
+#define ESB_IPC_H
+
+
+#define INVALID_MODE	0xFF
+#define	CONFIG_CENTRAL		1
+#define	CONFIG_PERIPH		2
+
+typedef enum
+{
+	APP_IDLE,
+	APP_CFG,
+	APP_OPT
+	
+	
+}app_state_t;
+
+typedef enum
+{
+	RADIO_INITIALIZE = 1,
+	RADIO_START_TX_POLL,
+	RADIO_START_RX_SCAN,
+	RADIO_FETCH_PACKET,
+	RADIO_CENTRAL_DATA_RCV,
+	RADIO_PERIPH_DATA_SND,
+	RADIO_PUT_PACKET
+
+}cmd_evt_t;
+
+typedef struct
+{
+    cmd_evt_t 	data_hdr;
+    uint8_t 	data_len;
+    uint8_t 	data[65];
+    
+}ipc_msg_t;
+
+#endif
